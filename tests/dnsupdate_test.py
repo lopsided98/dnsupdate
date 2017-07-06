d15e03f56d5b636b0c3b7fb8f0526bc44f8b0f9b
Ben Wolsieffer <benwolsieffer@gmail.com>
2017-07-06 19:50:24 -0400

    Format tests.

--- tests/dnsupdate_test.py
+++ tests/dnsupdate_test.py
@@ -1,9 +1,11 @@
-import dnsupdate
 import unittest
+
 from yaml import load
 
-class ConfigTest(unittest.TestCase):
+import dnsupdate
 
+
+class ConfigTest(unittest.TestCase):
     def _parse_address_provider_web(self, config):
         config = load(config)
         provider = dnsupdate._parse_address_provider(config)
@@ -19,7 +21,7 @@ class ConfigTest(unittest.TestCase):
                 ipv6_url: ipv6_test_url
         """
         self._parse_address_provider_web(config)
-        
+
     def test_parse_address_provider_web_quotes(self):
         config = """
             type: Web
@@ -40,7 +42,7 @@ class ConfigTest(unittest.TestCase):
             Web("ipv4_test_url", 'ipv6_test_url')
         """
         self._parse_address_provider_web(config)
-    
+
     def test_parse_address_provider_web_invalid_arg(self):
         config = load("""
             type: Web
@@ -48,7 +50,7 @@ class ConfigTest(unittest.TestCase):
                 invalid: invalid
         """)
         self.assertRaises(TypeError, dnsupdate._parse_address_provider, config)
-    
+
     def test_parse_address_provider_invalid_class(self):
         config = load("""
             type: InvalidClass
@@ -74,7 +76,7 @@ class ConfigTest(unittest.TestCase):
         """
         service, providers = self._parse_dns_service_staticurl(config)
         self.assertDictEqual(providers, dict())
-        
+
     def test_parse_dns_service_staticurl_shorthand(self):
         config = """
             StaticURL("ipv4_test_url", "ipv6_test_url")
@@ -97,7 +99,7 @@ class ConfigTest(unittest.TestCase):
         service, providers = self._parse_dns_service_staticurl(config)
         self.assertIsInstance(providers['ipv4'], dnsupdate.Web)
         self.assertIsNone(providers.get('ipv6'))
-        
+
     def test_parse_dns_service_staticurl_single_address_provider(self):
         config = """
             type: StaticURL
@@ -113,11 +115,11 @@ class ConfigTest(unittest.TestCase):
         service, providers = self._parse_dns_service_staticurl(config)
         self.assertIsInstance(providers['ipv4'], dnsupdate.Web)
         self.assertEqual(providers['ipv4'], providers['ipv6'])
-    
+
+
 class CacheTest(unittest.TestCase):
-    
     def test_load_empty_cache(self):
         cache = dnsupdate._load_cache('/invalid_dir/invalid_file.cache')
         self.assertListEqual(cache, list())
-        
+
 # vim: ts=4:ps=4:et
