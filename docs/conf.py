b829f7890bca584921043adb7cfae35709c44e46
Ben Wolsieffer <benwolsieffer@gmail.com>
2025-07-05 17:00:38 -0400

    Update copyright year

--- docs/conf.py
+++ docs/conf.py
@@ -38,7 +38,7 @@ master_doc = "index"
 
 # General information about the project.
 project = "dnsupdate"
-copyright = "2017, Ben Wolsieffer"
+copyright = "2025, Ben Wolsieffer"
 author = "Ben Wolsieffer"
 
 # The version info for the project you're documenting, acts as replacement for
